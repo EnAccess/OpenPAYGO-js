b4346a4576d74b9135329b9d4d009796e61a6f34
Beesafe <hikenike6@gmail.com>
2024-05-09 08:44:32 +0100

    initiate token module (#1)

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+
+const ADD_TIME = 1
+const SET_TIME = 2 
+const DISABLE_PAYG = 3
+const COUNTER_SYNC = 4
+const INVALID = 10
+const ALREADY_USED = 11
+
+
+module.exports = {
+    TokenTypes: {
+        ADD_TIME,
+        SET_TIME,
+        DISABLE_PAYG,
+        COUNTER_SYNC,
+        INVALID,
+        ALREADY_USED
+    }
+}
