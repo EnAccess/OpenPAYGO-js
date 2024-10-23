f9d3605914eb823373d2602fe378e910aefe71a5
mark8-1 <munyaarmel61@gmail.com>
2024-10-23 12:42:20 +0200

    deploy to github pages

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    assetPrefix: isProd ? 'OpenPAYGO-js' : '',
+    trailingSlash: true,
+    images: {
+      unoptimized: true, 
+    },
+
+};
 
 export default nextConfig;
